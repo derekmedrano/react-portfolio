3344fd3d40a4ac18e9b262c0a30dce3bb53562da
Derek Medrano <derekjmedrano@gmail.com>
2024-04-14 21:52:43 -0500

    TEMP COLOR THEME COMMIT BEFORE BIG CHANGES

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,7 +10,7 @@ function AboutMe() {
                     <img className='aboutme-photo' src='../public/assets/selfie.jpg' alt='profile picture' />
                 </div>
                 <div class='col-lg-6'>
-                    <h2>About Me</h2>
+                    <h2 className='title'>About Me</h2>
                     <div className='bio'>
                         <p> I'm a Jr Fullstack Developer with a knack for sleek and efficient <strong>UX/UI</strong>. I know how important clean design is and I put all my passion
                             creating beautiful, MOBILE-FIRST, applications. </p>
