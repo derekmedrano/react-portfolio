77cdb9d195b24c09f5290ebf07167e49e28ed67e
Derek Medrano <derekjmedrano@gmail.com>
2024-04-14 21:24:45 -0500

    created footer icons with font awesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
-function Footer(){
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () =>{
     return (
-        <p>Footer</p>
-    )}
+        <footer>
+        <ul className='footer'>
+          <li>
+            <a href='mailto:derekmedrano@gmail.com'>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/derekmedrano' target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+              
+            </a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/derek-medrano-9a3b15303/' target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+              
+            </a>
+          </li>
+        </ul>
+
+        </footer>
+    )
+}
 
 export default Footer
