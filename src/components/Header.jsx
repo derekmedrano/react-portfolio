2c900fa17fc85565981fe4c209724d7db2b9a7fb
Derek Medrano <derekjmedrano@gmail.com>
2024-04-14 20:47:15 -0500

    added more styling to header, trying to add gif but still in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <header>
             <h1 className='header'>derek medrano</h1>
+            <img src="../public/assets/portfolio_cat.gif" alt="GIF of a cat" className="gif-cat" />
         </header>
     )
 };
